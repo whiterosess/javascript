66795e543c38933e29491c0be680af9bad99db4d
whiterosess <69551907+whiterosess@users.noreply.github.com>
2020-09-14 08:24:56 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,63 @@
 console.log("Welcome to ES6 Study I!");
 console.log("Let's learn with Ken the Ninja!");
+
+let length=5;
+console.log (length);
+console.log (length*length*3);
+
+// Output the result of 5 plus 3
+console.log (5+3);
+// Output the result of 20 minus 8
+console.log (20-8);
+// Output the string "4 + 5"
+console.log ("4+5");
+
+// Output the result of 8 times 4
+console.log (8*4);
+// Output the result of 24 divided by 4
+console.log (24/4);
+// Output the remainder after dividing 7 by 2
+console.log (7%2);
+
+// Output the combination of "Master " and "White"
+console.log ("Master "+"White");
+// Output the combination of "20" and "15" (as a single string)
+console.log ("20"+"15");
+
+// Declare the name variable with the string value "Ken the Ninja"
+let name="Ken the Ninja";
+// Output the value of the name variable
+console.log (name);
+
+// Declare the length variable
+let length=5;
+// Output the value of the length variable
+console.log (length);
+// Use the length variable to output the circle's area
+console.log (length*length*3);
+
+let name = "Ken the Ninja";
+console.log(name);
+// Update the name variable with the value "Birdie"
+name = "Birdie";
+// Output the value of the name variable
+console.log (name);
+
+let number = 7;
+console.log(number);
+// Add 3 to the value of the number variable
+number+=3;
+console.log(number);
+// Divide the value of the number variable by 2
+number/=2;
+console.log(number);
+
+// Declare the language constant
+const language = "French";
+// Output the value of the language constant
+console.log (language);
+// Use the language constant to output "I can speak ____"
+console.log ("I can speak"+language);
+
+
+
